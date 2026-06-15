d8b97b8d564d592359e30e1021ebdcf8436097c3
VqleryCqlery <robloboiiiii@gmail.com>
2026-06-15 23:08:55 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+if (/Android|iPhone|iPad|BlackBerry|Windows Phone|Kindle/i.test(navigator.userAgent) === true) {
+	alert("not mobile friendly btw, im working on it though");
+}
+console.log("for debugging:", location, "\n\nhey fellow (probably) developer, how's it going? (no, you're not talking to an ai girlfriend)")
